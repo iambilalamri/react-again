97c991f84ef5a603df836283f371ae1f433e4ff2
Bilal AMRI <iambilalamri@gmail.com>
2021-07-04 00:39:40 +0200

    [develop]- build react app training

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useState } from "react";
+import Header from "./components/layout/Header";
+import Meals from "./components/meals/Meals";
+import Cart from "./components/cart/Cart";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <Fragment>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
